6299d417162ecc0eabff06f366c23939b376412c
Devlintonofrey <85979747+Devlintonofrey@users.noreply.github.com>
2021-06-25 21:30:30 +0000

    updated lines 2 through 34

--- main.cs
+++ main.cs
@@ -1,7 +1,33 @@
 using System;
 
-class MainClass {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+public class Program
+{
+	public static void Main()
+	{
+    /*temperature in celsius. If temperature is below freezing, display "This temperature is below freezing of 32 degree Farenheit" otherwise do nothing. 
+    If temperature is above boiling, display "This temperature is above boiling point of 212 degree Farenheit" otherwise do nothing.
+    */
+
+		double fahrenheit, celsius;
+
+		Console.WriteLine("Enter the Celsius value : ");
+		celsius = Convert.ToInt32(Console.ReadLine());
+
+		fahrenheit = (celsius * 9)/5 + 32;
+
+		Console.WriteLine("Fahrenheit : {0}",fahrenheit);
+
+        //Comparison >, <, ==, <=, >=
+    if (fahrenheit < 32 )
+    {
+      Console.WriteLine("This temperature is below freezing of 32 degree Farenheit");
+    }
+
+    if (fahrenheit > 212 )
+    {
+      Console.WriteLine("This temperature is above boiling");
+    }
+
+
+	}
 }
